00600f5e1861445f18fe56ae557613eec3c572a2
zbay <zlwilliams1@gmail.com>
2016-01-21 16:48:32 +0000

    Offsetting now works!

--- routes/search.js
+++ routes/search.js
@@ -3,14 +3,16 @@ require('dotenv').config();
 var request = require("request");
 var mongoose = require('mongoose');
 var baseURL = "https://api.imgur.com/3/gallery/search/?q=";
+var basePageURL = "https://i.imgur.com/";
 var Search = require(process.cwd() + "/dbmodels/searches.js");
 Search = mongoose.model("Search");
   app.get("/search/*", function(req, res) {
       var searchQuery = req.url.slice(req.url.indexOf("/search/")+8);
-      var offset = "" + searchQuery.slice(req.url.indexOf("?offset=") + 8);
+      var offset = searchQuery.slice(searchQuery.indexOf("?offset=") + 8);
       if(offset.length > 0){
-          searchQuery=searchQuery.slice(0, req.url.indexOf("?offset=") + 8);
+          searchQuery=searchQuery.slice(0, searchQuery.indexOf("?offset="));
       }
+      offset = parseInt(offset);
       var newSearch = new Search({"term": searchQuery});
        newSearch.save();
        request({
@@ -20,11 +22,18 @@ Search = mongoose.model("Search");
              'Authorization': 'Client-ID ' + process.env.CLIENT_ID
            }
            }, function(error, response, body){
-               if(!error && response.statusCode == 200){
-                   var data = JSON.parse(body.data);
-                   //make data more readeable and limit results to the offset number or 60, whichever's smaller
-                   //image_url: link, snippet: title, page_url: "http://i.imgur.com/" + id
-                   res.send(data);
+               var data = JSON.parse(body).data;
+               console.log(data);
+               if(!error){
+                   var bodyLength = data.length;
+                   if(offset > 0 && bodyLength > offset){
+                       bodyLength = offset;
+                   }
+                   var dataDisplay = [];
+                   for(var i = 0; i < bodyLength; i++){
+                       dataDisplay.push({"image_url": data[i].link, "title": data[i].title, "page_url": basePageURL + data[i].id});
+                   }
+                   res.send(dataDisplay);
                }
                else{
                    res.send(error);
